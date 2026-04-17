b23716e5427e4a38bb296e37931ca041d45de156
Kévio Castro <keviocastro@gmail.com>
2026-04-17 14:51:05 -0300

    chore: update API URL to local network IP for mobile testing

--- frontend/src/utils/socket.ts
+++ frontend/src/utils/socket.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 // Change this to your server IP address if testing on a physical device
-const SERVER_URL = 'http://localhost:4000';
+const SERVER_URL = 'http://192.168.157.140:4000';
 
 export const socket = io(SERVER_URL, {
   autoConnect: false,
